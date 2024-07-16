6d8de9acf79d0b835cf2d0bde1c78e3c02a51c27
Sajika2000 <95673193+Sajika2000@users.noreply.github.com>
2024-07-16 07:23:17 +0530

    create routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,28 @@
-
+import { BrowserRouter,Route,Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Product from "./pages/Product"
+import Contact from "./pages/Contact"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import Profile from "./pages/Profile"
 
 const App = () => {
   return (
-    <div className="bg-slate-200">
-      App
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element= {<Home/>}/>
+        <Route path="/about" element= {<About/>}/>
+        <Route path="/product" element= {<Product/>}/>
+        <Route path="/contact" element= {<Contact/>}/>
+        <Route path="/sign-in" element= {<Signin/>}/>
+        <Route path="/sign-up" element= {<Signup/>}/>
+        <Route path="/profile" element= {<Profile/>}/>
+        
+      </Routes>
+    
+    </BrowserRouter>
+    
   )
 }
 
