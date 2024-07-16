55201fe235cd495faa7885b9e275dfed835ab37d
Sajika2000 <95673193+Sajika2000@users.noreply.github.com>
2024-07-17 00:56:47 +0530

    create and run the server

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,10 +1,12 @@
 import AboutHero from "../components/AboutHero"
+import Testimonials from "../components/Testimonials"
 
 
 const About = () => {
   return (
     <div>
       <AboutHero/>
+      <Testimonials/>
     </div>
   )
 }
